2b7b7c110bffcb437edd6f1d0b5a51f9b30e518a
Looi Yue Peng <marcusheadgotscarm16@gmail.com>
2021-10-25 02:32:03 +0800

    added stock data structure

---
+++ src/pos/Stock.java
@@ -0,0 +1,33 @@
+package pos;
+
+public class Stock {
+    private String stockID;
+    private String name;
+    private String quantity;
+
+    public String getStockID() {
+        return this.stockID;
+    }
+
+    public void setStockID(String stockID) {
+        this.stockID = stockID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+    
+
+}
