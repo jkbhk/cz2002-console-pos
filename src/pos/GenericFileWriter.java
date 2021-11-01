7759e13fc746f83153ad34e37a033573169afa70
Looi Yue Peng <marcusheadgotscarm16@gmail.com>
2021-11-02 02:39:34 +0800

    added support for writing 2d array

--- src/pos/GenericFileWriter.java
+++ src/pos/GenericFileWriter.java
@@ -2,11 +2,64 @@ package pos;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class GenericFileWriter {
     
     
     private static String filePathSuffix = "../data/";
+    private static final char DELIMITTER = ',';
+
+    public static void writeFile(String[][] items,String filename){
+        try (PrintWriter writer = new PrintWriter(filePathSuffix + filename)) {
+
+            StringBuilder sb = new StringBuilder();
+
+            for(String[] item : items){
+
+                sb.append(IDGenerator.GenerateUniqueID());
+
+                for(String p : item){
+                    sb.append(DELIMITTER);
+                    sb.append(p);
+                }
+
+                sb.append('\n');
+            }
+
+            
+            /*sb.append("Id");
+            sb.append(',');
+            sb.append("Name");
+            sb.append('\n');
+      
+            sb.append(IDGenerator.GenerateUniqueID());
+            sb.append(',');
+            sb.append("Chicken rice");
+            sb.append('\n');
+      
+            
+            sb.append(IDGenerator.GenerateUniqueID());
+            sb.append(',');
+            for(int i = 0; i<properties.length;i++){
+                
+                sb.append(properties[i]);
+                sb.append(',');
+            }
+            sb.append('\n');
+            */
+
+
+            writer.write(sb.toString());
+      
+            System.out.println("write complete!");
+      
+          } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+          }
+    }
+
+
 
     public static void writeFile(String[] properties,String filename){
         try (PrintWriter writer = new PrintWriter(filePathSuffix + filename)) {
