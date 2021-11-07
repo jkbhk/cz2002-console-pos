cc05d1edad014ed602c415eddadb4409a516ecef
Eric <eric23july@hotmail.com>
2021-11-08 00:07:30 +0800

    Added Staff and StaffManager

---
+++ src/pos/Staff.java
@@ -0,0 +1,36 @@
+package pos;
+
+public class Staff {
+	
+	private String staffName;
+	private int employeeID;
+	private String gender;
+	private String jobTitle;
+	
+	public String getStaffName() {
+		return staffName;
+	}
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	
+	
+}
